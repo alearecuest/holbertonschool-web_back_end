e0d9dd0be113617fe467103d4c33b8e55ab990a9
Alejandro <10653@holbertonstudents.com>
2025-07-29 10:12:21 -0300

    Function is annotated

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,7 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronously generates ten random floats between 0 and 10,
     pausing for one second before yielding each value.
