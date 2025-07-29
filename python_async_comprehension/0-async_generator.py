1d2f6feed1c0159ccf8b652d43901137f726b933
Alejandro <10653@holbertonstudents.com>
2025-07-29 10:15:44 -0300

    More errors

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ between each yield.
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
