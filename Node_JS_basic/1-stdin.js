24d16244fee328343ef81bd526746a71181643a3
Alejandro <10653@holbertonstudents.com>
2025-09-02 18:23:48 -0300

    Update errors

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-let inputData = '';
-
 process.stdin.on('data', (data) => {
-  inputData += data.toString();
-  
-  if (process.stdin.isTTY) {
-    const name = inputData.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
-  }
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\r\n`);
 });
 
 process.stdin.on('end', () => {
-  if (inputData) {
-    const name = inputData.trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-  }
   process.stdout.write('This important software is now closing\n');
 });
