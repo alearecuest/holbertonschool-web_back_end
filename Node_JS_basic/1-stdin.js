9bb44b88df4f46b2fe5658a78eaa2e4f1b00d6a3
Alejandro <10653@holbertonstudents.com>
2025-09-02 18:26:58 -0300

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\r\n`);
-});
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  process.stdout.write(`Your name is: ${data}`);
+  if (process.stdin.isTTY) {
+    process.exit();
+  } else {
+    process.stdout.write('This important software is now closing\n');
+    process.exit();
+  }
 });
