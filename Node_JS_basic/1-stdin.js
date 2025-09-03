dfcb36bff4f16884ea390fd5bab0e3f95852557c
Alejandro <10653@holbertonstudents.com>
2025-09-03 13:39:22 -0300

    1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  
   if (process.stdin.isTTY) {
     process.exit();
   }
