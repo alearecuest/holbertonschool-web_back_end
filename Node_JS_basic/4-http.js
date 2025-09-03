fcc28c01e6cded17434f93b323bb346126d56a1d
Alejandro <10653@holbertonstudents.com>
2025-09-03 18:25:54 -0300

    Update errors

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,9 +1,9 @@
 const http = require('http');
 
 const app = http.createServer((request, response) => {
-	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/plain');
-	response.end('Hello Holberton School!');
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
 });
 
 app.listen(1245);
