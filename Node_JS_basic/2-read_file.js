c65f63261a86e5757ab7795e328139a78cfacee2
Alejandro <10653@holbertonstudents.com>
2025-09-03 15:45:24 -0300

    Update style

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -8,7 +8,7 @@ function countStudents(path) {
       .trim()
       .split('\n')
       .filter((line) => line.trim() !== '');
-    
+
     if (lines.length <= 1) {
       console.log('Number of students: 0');
       return;
