8b37376ebc22e8f2ad69841b4ded6c7c6498b093
Alejandro <10653@holbertonstudents.com>
2025-09-06 18:12:50 -0300

    Update errors

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -5,40 +5,38 @@ const app = express();
 const port = 1245;
 const databaseFile = process.argv[2] || 'database.csv';
 
-const countStudents = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+
+    const totalStudents = students.length;
+
+    const fields = {};
+    for (const student of students) {
+      const studentData = student.split(',');
+      const field = studentData[3];
+      
+      if (!fields[field]) {
+        fields[field] = {
+          count: 0,
+          list: [],
+        };
       }
 
-      const lines = data.split('\n').filter(line => line.trim() !== '');
-      
-      const students = lines.slice(1);
-      
-      const totalStudents = students.length;
-      
-      const fields = {};
-      for (const student of students) {
-        const studentData = student.split(',');
-        const field = studentData[3];
-        
-        if (!fields[field]) {
-          fields[field] = {
-            count: 0,
-            list: [],
-          };
-        }
-        
-        fields[field].count += 1;
-        fields[field].list.push(studentData[0]);
-      }
-      
-      resolve({ totalStudents, fields });
-    });
+      fields[field].count += 1;
+      fields[field].list.push(studentData[0]);
+    }
+
+    resolve({ totalStudents, fields });
   });
-};
+});
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
@@ -50,12 +48,12 @@ app.get('/students', (req, res) => {
   countStudents(databaseFile)
     .then(({ totalStudents, fields }) => {
       res.write(`Number of students: ${totalStudents}\n`);
-      
+
       for (const [field, data] of Object.entries(fields)) {
         const list = data.list.join(', ');
         res.write(`Number of students in ${field}: ${data.count}. List: ${list}\n`);
       }
-      
+
       res.end();
     })
     .catch((error) => {
